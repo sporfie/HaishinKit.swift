2845bd1843f9a3b65e6c58744602d145a378f82e
Guy Moreillon <guy.moreillon@brozermo.com>
2020-11-13 14:33:56 +0100

    Added BlueSocket dependency in Package description

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     targets: [
         .target(name: "SwiftPMSupport"),
-        .target(name: "HaishinKit", dependencies: ["Logboard", "SwiftPMSupport"],
+        .target(name: "HaishinKit", dependencies: ["Logboard", "SwiftPMSupport", "BlueSocket"],
                 path: "Sources",
                 sources: [
                     "Codec",
