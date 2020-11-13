e232e893659b99857968e0d20f65ff875d864eea
Guy Moreillon <guy.moreillon@brozermo.com>
2020-11-13 10:00:30 +0100

    Added outdated packets discarding

--- Sources/RTMP/RTMPBlueSocket.swift
+++ Sources/RTMP/RTMPBlueSocket.swift
@@ -8,9 +8,10 @@
 
 import Socket
 
-final class RTMPBlueSocket: RTMPSocketCompatible {
-	let writeTimeOut : UInt = 300
-    var timestamp: TimeInterval = 0.0
+open class RTMPBlueSocket: RTMPSocketCompatible {
+	open var writeTimeOut = 500.0
+
+	var timestamp: TimeInterval = 0.0
     var chunkSizeC: Int = RTMPChunk.defaultSize
     var chunkSizeS: Int = RTMPChunk.defaultSize
     var windowSizeC = Int(UInt8.max)
@@ -25,13 +26,16 @@ final class RTMPBlueSocket: RTMPSocketCompatible {
     var inputBuffer = Data()
     weak var delegate: RTMPSocketDelegate?
 
-    private(set) var queueBytesOut: Atomic<Int64> = .init(0)
-    private(set) var totalBytesIn: Atomic<Int64> = .init(0)
-    private(set) var totalBytesOut: Atomic<Int64> = .init(0)
-    private(set) var connected = false {
+    var queueBytesOut: Atomic<Int64> = .init(0)
+    var totalBytesIn: Atomic<Int64> = .init(0)
+    var totalBytesOut: Atomic<Int64> = .init(0)
+    var totalBytesDiscarded: Atomic<Int64> = .init(0)
+    var connected = false {
         didSet {
             if connected {
-                doOutput(data: handshake.c0c1packet)
+				outputQueue.async {
+					self.send(data: self.handshake.c0c1packet)
+				}
                 readyState = .versionSent
                 return
             }
@@ -42,9 +46,9 @@ final class RTMPBlueSocket: RTMPSocketCompatible {
             events.removeAll()
         }
     }
-    private var events: [Event] = []
-    private var handshake = RTMPHandshake()
-    private var connection: Socket? {
+    var events: [Event] = []
+    var handshake = RTMPHandshake()
+    var connection: Socket? {
         didSet {
             oldValue?.close()
             if connection == nil {
@@ -52,8 +56,8 @@ final class RTMPBlueSocket: RTMPSocketCompatible {
             }
         }
     }
-    private lazy var inputQueue = DispatchQueue(label: "com.haishinkit.HaishinKit.NWSocket.input", qos: qualityOfService)
-    private lazy var outputQueue = DispatchQueue(label: "com.haishinkit.HaishinKit.NWSocket.output", qos: qualityOfService)
+    lazy var inputQueue = DispatchQueue(label: "com.haishinkit.HaishinKit.NWSocket.input", qos: qualityOfService)
+    lazy var outputQueue = DispatchQueue(label: "com.haishinkit.HaishinKit.NWSocket.output", qos: qualityOfService)
 
     func connect(withName: String, port: Int) {
         handshake.clear()
@@ -63,13 +67,13 @@ final class RTMPBlueSocket: RTMPSocketCompatible {
         totalBytesIn.mutate { $0 = 0 }
         totalBytesOut.mutate { $0 = 0 }
         queueBytesOut.mutate { $0 = 0 }
+        totalBytesDiscarded.mutate { $0 = 0 }
         inputBuffer.removeAll(keepingCapacity: false)
 		connection = try? Socket.create()
 		guard connection != nil else { return }
 		inputQueue.async {
 			try? self.connection!.connect(to: withName, port: Int32(port), timeout: UInt(self.timeout*1000))
 			if self.connection!.isConnected {
-//				try? self.connection?.setWriteTimeout(value: self.writeTimeOut)
 				self.connected = true
 				self.receive()
 			} else {
@@ -91,11 +95,22 @@ final class RTMPBlueSocket: RTMPSocketCompatible {
 
     @discardableResult
     func doOutput(chunk: RTMPChunk, locked: UnsafeMutablePointer<UInt32>? = nil) -> Int {
-        let chunks: [Data] = chunk.split(chunkSizeS)
-        for i in 0..<chunks.count - 1 {
-            doOutput(data: chunks[i])
-        }
-        doOutput(data: chunks.last!, locked: locked)
+		let queuedAt = CACurrentMediaTime()
+        outputQueue.async {
+			let then = CACurrentMediaTime()
+			let queuedFor = (then-queuedAt)*1000
+			guard queuedFor < self.writeTimeOut else {
+				self.totalBytesDiscarded.mutate { $0 += Int64(chunk.data.count) }
+				print("discarded \(chunk.data.count) after \(Int(queuedFor))")
+				return
+			}
+			
+			let chunks: [Data] = chunk.split(self.chunkSizeS)
+			for i in 0..<chunks.count - 1 {
+				self.send(data: chunks[i])
+			}
+			self.send(data: chunks.last!, locked: locked)
+		}
         if logger.isEnabledFor(level: .trace) {
             logger.trace(chunk)
         }
@@ -103,28 +118,21 @@ final class RTMPBlueSocket: RTMPSocketCompatible {
     }
 
     @discardableResult
-    func doOutput(data: Data, locked: UnsafeMutablePointer<UInt32>? = nil) -> Int {
+    func send(data: Data, locked: UnsafeMutablePointer<UInt32>? = nil) -> Int {
+		guard connected else { return 0 }
         queueBytesOut.mutate { $0 += Int64(data.count) }
-        outputQueue.async {
-			do {
-				let then = CACurrentMediaTime()
-				let count = try self.connection?.write(from: data)
-				//			if count == 0 {
-				//				self.close(isDisconnected: true)
-				//				return
-				//			}
-				let elapsed = Int((CACurrentMediaTime()-then)*1000)
-				if elapsed > self.writeTimeOut || (count == 0 && errno == EAGAIN) {
-					print("*********** sent \(count!) \(elapsed)")
-				} else {
-					print("sent \(count ?? -100) in \(elapsed)")
-				}
-			} catch {
-				print(error)
+		do {
+			let then = CACurrentMediaTime()
+			let count = try self.connection?.write(from: data)
+			let elapsed = (CACurrentMediaTime()-then)*1000
+			if elapsed > self.writeTimeOut {
+				print("*********** sent \(count!) \(Int(elapsed))")
 			}
-			self.totalBytesOut.mutate { $0 += Int64(data.count) }
-			self.queueBytesOut.mutate { $0 -= Int64(data.count) }
-        }
+		} catch {
+			print(error)
+		}
+		self.totalBytesOut.mutate { $0 += Int64(data.count) }
+		self.queueBytesOut.mutate { $0 -= Int64(data.count) }
         return data.count
     }
 
@@ -147,9 +155,12 @@ final class RTMPBlueSocket: RTMPSocketCompatible {
             if inputBuffer.count < RTMPHandshake.sigSize + 1 {
                 break
             }
-            doOutput(data: handshake.c2packet(inputBuffer))
-            inputBuffer.removeSubrange(0...RTMPHandshake.sigSize)
-            readyState = .ackSent
+			let data = handshake.c2packet(inputBuffer)
+			outputQueue.async {
+				self.send(data: data)
+			}
+			inputBuffer.removeSubrange(0...RTMPHandshake.sigSize)
+			readyState = .ackSent
             if RTMPHandshake.sigSize <= inputBuffer.count {
                 listen()
             }
