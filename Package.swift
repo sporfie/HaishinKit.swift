b19e24bf1ada94c665400823d43745c0f3cfbac5
Guy Moreillon <guy.moreillon@brozermo.com>
2020-11-13 14:28:24 +0100

    Added BlueSocket dependency in Package description

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/shogo4405/Logboard.git", from: "2.2.1")
+		.package(url: "https://github.com/Kitura/BlueSocket.git", from: "1.0.200")
     ],
     targets: [
         .target(name: "SwiftPMSupport"),
