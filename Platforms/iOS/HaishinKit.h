8a1581bb1e6ea2ecc52293e9db6efde82499f424
Guy Moreillon <guy.moreillon@brozermo.com>
2021-12-02 14:48:35 +0100

    Expose SRT lib in exported symbols

--- Platforms/iOS/HaishinKit.h
+++ Platforms/iOS/HaishinKit.h
@@ -1,5 +1,7 @@
 #import <UIKit/UIKit.h>
 
+#import "../../../Vendor/SRT/srt/srtcore/srt.h"
+
 static NSString *const __nonnull HaishinKitIdentifier = @"com.haishinkit.HaishinKit";
 FOUNDATION_EXPORT double HaishinKitVersionNumber;
 FOUNDATION_EXPORT const unsigned char HaishinKitVersionString[];
